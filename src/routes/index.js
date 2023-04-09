b92de838b600db9c77289159e905c123b3d85487
mtavidal <119874263+mtavidal@users.noreply.github.com>
2023-04-09 19:52:53 -0300

    adiciona controller e rotas do Dashboard - faltando implementar funções que dependem de Atendimento

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 import health from './health/healthRoute.js';
 import psicologos from './psicologos/psicologosRoute.js';
 import pacientes from './pacientes/pacientesRoute.js';
+import dashboard from './dashboard/dashboardRoute.js';
 
 export default (app) => {
     app.use(health);
     app.use(psicologos);
     app.use(pacientes);
+    app.use(dashboard);
 };
